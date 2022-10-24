af8339a008afc090c4c14854dfa5f5342fd7ba91
nicoseng <nicolas.sengmany@gmail.com>
2022-10-24 13:18:10 +0200

    ajout export ENV=production

--- purbeurre_project/settings.py
+++ purbeurre_project/settings.py
@@ -21,13 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = config('SECRET_KEY')
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False if os.environ.get("ENV", "development") == "production" else True
 
-ALLOWED_HOSTS = ["127.0.0.1", "purbeurre-website.herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "purbeurre-website.herokuapp.com", "localhost"]
 
 # Application definition
 
