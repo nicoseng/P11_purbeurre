3dcf34b2a30872dabe3d6b2c98a0e1b199d06ba0
nicoseng <nicolas.sengmany@gmail.com>
2022-10-25 11:49:19 +0200

    ajout modif selenium

--- purbeurre_website/tests/functional_tests/test_authentication.py
+++ purbeurre_website/tests/functional_tests/test_authentication.py
@@ -10,9 +10,8 @@ from selenium.webdriver.chrome.service import Service
 
 class TestAuthentication(LiveServerTestCase):
     def setUp(self):
-        # service = Service('/Users/nicolassengmany/Desktop/OCR/Python/Projets/P10_purbeurre/purbeurre_website'
-        #                   '/tests/functional_tests/chromedriver')
-        service = Service('/home/travis/build/nicoseng/P10_purbeurre/purbeurre_website/tests/functional_tests/')
+        service = Service('/Users/nicolassengmany/Desktop/OCR/Python/Projets/P10_purbeurre/purbeurre/purbeurre_website'
+                          '/tests/functional_tests/chromedriver')
         self.browser = webdriver.Chrome(service=service)
         self.browser.maximize_window()
 
