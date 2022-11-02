76d564b367bc7468e24bc91a4d3d6e3c1e3ecf1c
nicoseng <nicolas.sengmany@gmail.com>
2022-11-02 16:29:32 +0100

    ajout modif browser options

--- purbeurre_website/tests/functional_tests/test_authentication.py
+++ purbeurre_website/tests/functional_tests/test_authentication.py
@@ -6,6 +6,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
 
 class TestAuthentication(LiveServerTestCase):
